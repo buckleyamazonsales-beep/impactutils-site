c5a369f3db5d0f19c738c4592c2b212b95311aa0
NotttttYessss2222 <Matt.buckley16@hotmail.com>
2026-04-22 21:47:24 -0400

    Move server secrets to deploy secrets

--- api/stripe-secrets.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'STRIPE_SECRET_KEY' => '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',
-    'STRIPE_WEBHOOK_SECRET' => 'whsec_kOzIPnv3MBLFBL5R2pXtToSvyBb1bTDJ',
-    'STRIPE_PRICE_PRO' => 'price_1TLvfKC7xlHhzPa5ZK5nvqgl',
-    'STRIPE_PRICE_FOUNDER' => 'price_1TLvfHC7xlHhzPa5be41Ev1d',
-    'IMPACT_PUBLIC_SITE_URL' => 'https://impactutils.com',
-    'IMPACT_ADMIN_API_KEY' => '',
-];
